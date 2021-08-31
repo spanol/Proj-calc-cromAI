b4e822acbf2f0a8a39cd07d5fdb00bcf0fa4396c
code_spanol <viniciusspanol@gmail.com>
2021-08-31 20:55:40 -0300

    2055, ainda restam algums detalhes na calculadora e upgrades no css.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,34 +1,37 @@
-import React, {useState} from 'react';
+
 import "./App.css"
 
 const App = () =>{
-  var [Hipotenusa,Cateto_1,Cateto_2] = useState("");
 
-  function Pitagorar() {
-    Cateto_1 = document.getElementById('Cateto_1');
-    Cateto_2 = document.getElementById('Cateto_2');
-    var Hipotenusa = Math.sqrt(Math.pow(Cateto_1,2)+Math.pow(Cateto_2,2))
-   document.getElementById('Hipotenusa').write(Hipotenusa)
+  function Calcular_Hipotenusa() {
+   var Cateto_1 = parseInt(document.getElementById('Cateto_1'));
+   var Cateto_2 = parseInt(document.getElementById('Cateto_2'));
+   var Hipotenusa = Math.sqrt(Math.pow(Cateto_1,2)+Math.pow(Cateto_2,2))
+   document.getElementById('Hipotenusa').innerHTML="The Length of hypotenuse is= "+Hipotenusa;
   }
 
-
- 
+  
 
   return (
      <>
-     <main>
-         <div className='entradas'>
-           <form>
-             <input name='Cateto_1' type='number' placeholder='Valor do cateto oposto'></input>
-             <input name='Cateto_2' type='number' placeholder='Valor do cateto adjacente'></input>
-             <input name='Hipotenusa' type='number' placeholder='Valor da hipotenusa' value={Hipotenusa}></input>
-           </form>
-         </div>
-         <div className='Botões de ação'>
-           <button>Limpar</button>
-           <button onClick={Pitagorar} >Calcular</button>
-         </div>
-      </main>
+     <nav><ul>Boas vindas, caro Pitagoras!</ul></nav>
+     <aside id="left">Neste pergaminho, vou mostrar-lhe todo o poder do fruto de vosso esforço...</aside>
+     <header>Calculadora do teorema de pitagoras:</header>
+     <section id="Calculadora">
+       <article>Digite o valor de vossos catetos e retornarei-te vossa hipotenusa.</article>
+       <div id="Entradas">
+       <form>
+       <input id='Cateto_1' type='number' placeholder='Valor do cateto oposto'></input>
+       <input id='Cateto_2' type='number' placeholder='Valor do cateto adjacente'></input>
+       <p id='Hipotenusa'></p>
+       </form>
+       </div>
+       <div id="Botões de ação">
+       <button type="reset">Limpar</button>
+       <button type="submit" onClick={Calcular_Hipotenusa} >Calcular</button>
+       </div>
+     </section>
+     <footer></footer>
      </>
   );
 }
